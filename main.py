71e70b19db2bbd4c39667d44344e74df57b22c9f
知行合一 <tom_9163@outlook.com>
2020-02-18 09:19:56 +0800

    ·Change technology.html link target

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ app = Flask(__name__)
 @app.route('/pages/<url1>/<url2>')
 def getPages(url1, url2):
     return render_template('pages/origin.html',
-                           title=url2[:url2.__len__()],
+                           title=url2[:url2.__len__()-5],
                            content='pages/{}/{}'.format(url1, url2))
 
 @app.route('/pages/<url>')
 def getRootPages(url):
+    en2zhDict = {
+        'introduction.html': '活动介绍',
+        'recruit.html': '招新计划',
+        'technology.html': '技术服务'
+    }
+
     return render_template('pages/origin.html',
-                           title=url[:url.__len__()],
+                           title=en2zhDict[url],
                            content='pages/{}'.format(url))
 
 @app.route('/')
@@ -19,4 +25,4 @@ def getIndex():
     return render_template('pages/origin.html', content='index.html')
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
